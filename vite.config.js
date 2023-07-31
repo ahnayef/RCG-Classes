eb88fccf55b1bfa95ba10de4844648e09a314090
AHNayef <ahnayef@outlook.com>
2023-08-01 00:18:46 +0600

    Update images

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import { VitePWA } from "vite-plugin-pwa";
 
 const manifestForPlugin = {
   registerType: "prompt",
-  includeAssets: ["favicon.ico", "pwa-icon-512.png", "android-chrome-512x512.png", "apple-touch-icon.png", "mask-icon.svg", "cr.jpg"],
+  includeAssets: ["favicon.ico", "pwa-icon-512.png", "android-chrome-256x256.png", "apple-touch-icon.png", "mask-icon.svg", "cr.jpg"],
   manifest: {
     name: "Routine | Spring 23",
     short_name: "Routine",
     description: "Spring23 routine app",
     icons: [
       {
-        src: "src/assets/img/android-chrome-512x512.png",
-        sizes: "512x512",
+        src: "src/assets/img/android-chrome-265x256.png",
+        sizes: "256x256",
         type: "image/png",
         purpose: "maskable",
       },
@@ -30,7 +30,7 @@ const manifestForPlugin = {
       },
       {
         src: "src/assets/img/mask-icon.svg",
-        sizes: "144x65",
+        sizes: "81x113",
         type: "image/svg+xml",
         purpose: "any maskable",
       },
